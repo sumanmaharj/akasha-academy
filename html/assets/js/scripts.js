f2aa8024b7c9dc1312a6367fb2e6053b9e5d1baf
suman <sumanmaharj@gmail.com>
2022-10-28 17:26:42 +0545

    initialize upload project

---
+++ html/assets/js/scripts.js
@@ -0,0 +1,39 @@
+function testimonial(){
+    const swiper = new Swiper('#testimonials', {
+        // Optional parameters
+        slidesPerView: 3,
+      
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      
+        // And if we need scrollbar
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+      });
+}
+
+function video(){
+  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+      disableOn: 700,
+      type: 'iframe',
+      mainClass: 'mfp-fade',
+      removalDelay: 160,
+      preloader: false,
+
+      fixedContentPos: false
+  });
+}
+$(document).ready(function(){
+    testimonial();
+    video();
+
+})
